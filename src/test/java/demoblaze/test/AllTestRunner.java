01bbb705ccfdd7ff2cac9b0c8146d4fa8e1b2327
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2021-05-04 13:32:18 +0200

    Update AllTestRunner.java

--- src/test/java/demoblaze/test/AllTestRunner.java
+++ src/test/java/demoblaze/test/AllTestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features/",
-        glue = "demoblaze.test/",
+        glue = {"demoblaze.test/navigationbar/","demoblaze.test/login/","demoblaze.test/cart/", "demoblaze.test/setup/"},
         plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
                 "junit:target/cucumber-reports/Cucumber.xml",
                 "html:target/cucumber-reports" },
